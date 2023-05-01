6df872f1c8df5fcf632210f55047d4c2545efe38
Amin Mustafa <muztafawi@gmail.com>
2023-05-01 21:31:22 +0300

    Added header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="text-[#a0a0a0] flex justify-between items-center text-sm">
+      <div>Pomodeep</div>
+      <div>Current Time</div>
+      <div>Like It 👍</div>
+    </div>
+  );
 };
 
 export default Header;
